ed2793e92416e1df6faee9d1e72a0572cf7cea04
JaeseoKim <169111622+JseSeo@users.noreply.github.com>
2024-10-06 19:12:02 +0900

    Delete src/main/java/jeiu/capstone/jongGangHaejo directory
    잡다한거 삭제

--- src/main/java/jeiu/capstone/jongGangHaejo/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package jeiu.capstone.jongGangHaejo.repository;
-
-import jeiu.capstone.jongGangHaejo.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
