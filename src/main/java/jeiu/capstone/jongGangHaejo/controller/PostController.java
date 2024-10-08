88258da3a880c7d65f60f6fc93fffde903d2063f
dongjun <djl020326@gmail.com>
2024-10-08 22:33:52 +0900

    PostControllerTest S3 업로드에 맞게 삭제, PostController /test들 삭제

--- src/main/java/jeiu/capstone/jongGangHaejo/controller/PostController.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/controller/PostController.java
@@ -29,29 +29,6 @@ public class PostController {
         return "안녕하세요 종강해조 프로젝트입니다";
     }
 
-    @PostMapping("/test")
-    public String postTest() {
-        return "안녕하세요 종강해조 프로젝트입니다";
-    }
-
-/*
-    @PostMapping("/test")
-    public String postTest(@RequestParam String title, @RequestParam String content) {
-        log.info("title={}, content={}", title, content);
-        return "Hello World";
-    }
- */
-
-/*
-    @PostMapping("/test")  //Map을 이용하여 넘기는 방법도 있다
-    public String postTest(@RequestParam Map<String, String> params) {
-        log.info("params={}", params);
-        String title = params.get("title"); //추후 맵에서 꺼내 사용할 수도 있다
-        return "Hello World";
-    }
-    //클래스를 정의하여 넘기는 방법이 조금 더 유지보수 하기 좋아보인다 -> dto.request 패키지에 정의하겠음
- */
-
     //DTO를 통해 값을 가져오는 방식
 
 
--- src/test/java/jeiu/capstone/jongGangHaejo/controller/PostControllerTest.java
+++ src/test/java/jeiu/capstone/jongGangHaejo/controller/PostControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,57 +32,16 @@ class PostControllerTest {
     @MockBean
     private S3Template s3Template;
 
-//    @Test
-//    @DisplayName("GET /test - Hello World")
-//    void Get_HelloWorld_테스트() throws Exception {
-//        // expected
-//        mockMvc.perform(MockMvcRequestBuilders.get("/test")) // /test로 요청시
-//                .andExpect(status().isOk()) //HTTP 상태코드가 200이여야 하며
-//                .andExpect(content().string("안녕하세요 종강해조 프로젝트입니다")) //Hello World로 내려지는 것을 기대한다
-//                .andDo(print()); //HTTP요청 정보를 출력되게 함
-//        //status, content, print등 static import진행 - MockMvcResultMatchers의 메서드들이다.
-//    }
-//
-//    @Test
-//    @DisplayName("POST /test - Hello World")
-//    void Post_HelloWorld_테스트() throws Exception {
-//        // expected
-//        mockMvc.perform(post("/test"))
-//                .andExpect(status().isOk()) //HTTP 상태코드가 200이여야 하며
-//                .andExpect(content().string("안녕하세요 종강해조 프로젝트입니다")) //Hello World로 내려지는 것을 기대한다
-//                .andDo(print()); //mockMVC를 통해 요청한 HTTP요청 정보를 출력되게 함
-//    }
-//
-//    @Test
-//    @DisplayName("게시글_요청_JSON_제목 검증")
-//    void 게시물_제목_검증() throws Exception {
-//        // expected
-//        mockMvc.perform(post("/post")
-//            .contentType(MediaType.APPLICATION_JSON) //application/json 타입으로
-//            .content("{\"title\": \"\", \"content\": \"내용\"}") //해당 JSON형식으로 요청
-//        )
-//
-//        .andExpect(status().isBadRequest()) //HttpSatus는 400이여야 함
-//
-//        //ExceptionHanlingController에서 handling 잘 되는지 확인
-//        .andExpect(jsonPath("$.code").value("400"))
-//        .andExpect(jsonPath("$.message").value("잘못된 요청 입니다 validation을 참고해주세요"))
-//        .andDo(print()); //mockMVC를 통해 요청한 HTTP요청 정보를 출력되게 함
-//    }
-//
-//    @Test
-//    @DisplayName("게시글_요청_JSON_내용 검증")
-//    void 게시물_내용_검증() throws Exception {
-//        mockMvc.perform(post("/post")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content("{\"title\": \"제목\", \"content\": \"\"}")
-//        )
-//
-//        .andExpect(status().isBadRequest())
-//        .andExpect(jsonPath("$.code").value("400"))
-//        .andExpect(jsonPath("$.message").value("잘못된 요청 입니다 validation을 참고해주세요"))
-//        .andDo(print());
-//    }
+    @Test
+    @DisplayName("GET /test - Hello World")
+    void Get_HelloWorld_테스트() throws Exception {
+        // expected
+        mockMvc.perform(MockMvcRequestBuilders.get("/test")) // /test로 요청시
+                .andExpect(status().isOk()) //HTTP 상태코드가 200이여야 하며
+                .andExpect(content().string("안녕하세요 종강해조 프로젝트입니다")) //Hello World로 내려지는 것을 기대한다
+                .andDo(print()); //HTTP요청 정보를 출력되게 함
+        //status, content, print등 static import진행 - MockMvcResultMatchers의 메서드들이다.
+    }
 
     @Test
     @DisplayName("파일_업로드_및_게시물_생성_테스트")
