998bda424413ab0d4f594905cf39292b021164f4
dong-junn <djl020326@gmail.com>
2024-10-15 15:07:26 +0900

    PostDocTest : 사용하지 않는 import 삭제

--- src/test/java/jeiu/capstone/jongGangHaejo/controller/PostDocTest.java
+++ src/test/java/jeiu/capstone/jongGangHaejo/controller/PostDocTest.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
