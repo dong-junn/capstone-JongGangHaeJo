bef1024402380d878c968855461708e6926a567b
dong-junn <djl020326@gmail.com>
2024-10-15 14:40:59 +0900

    수정된 어플리케이션 코드에 맞게 테스트코드 변경(POST 엔티티)

--- src/test/java/jeiu/capstone/jongGangHaejo/repository/RepositoryTest.java
+++ src/test/java/jeiu/capstone/jongGangHaejo/repository/RepositoryTest.java
@@ -4,11 +4,13 @@ import jeiu.capstone.jongGangHaejo.domain.Post;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class RepositoryTest {
 
     @Autowired
@@ -17,13 +19,13 @@ public class RepositoryTest {
     @Test
     @DisplayName("게시물 DB에 저장되는지 확인")
     void PostSaveTest() {
-        //todo: 테스트코드 수정해줘 우진
-        
-        
         //given -> 이러한 자원이 주어졌을때
         Post post = Post.builder() //Builder 패턴 사용
+                .username("test")
                 .title("제목")
                 .content("내용")
+                .team("team A")
+                .youtubelink("https://youtube.com/example")
                 .build(); //new Post(title, content)와 같다
         //when -> 이러한 로직을 수행 후
         Post savedPost = postRepository.save(post); //JPA를 이용해 저장
@@ -32,5 +34,7 @@ public class RepositoryTest {
         assertThat(savedPost.getPostid()).isNotNull(); //Id가 있는지 확인 후
         assertThat(savedPost.getTitle()).isEqualTo("제목"); //title에 제목이 저장됐는지 확인 후
         assertThat(savedPost.getContent()).isEqualTo("내용"); //content에 내용이 저장됐는지 확인한다
+        assertThat(savedPost.getUsername()).isEqualTo("test");
+        assertThat(savedPost.getCreatedAt()).isNotNull();
     }
 }
