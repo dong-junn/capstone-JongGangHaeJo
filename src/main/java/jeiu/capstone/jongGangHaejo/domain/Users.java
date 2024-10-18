375cbee1964f3f8d05c47409ebed7939a4c6c6df
dong-junn <djl020326@gmail.com>
2024-10-18 11:09:23 +0900

    회원가입을 위한 Users Entity

---
+++ src/main/java/jeiu/capstone/jongGangHaejo/domain/Users.java
@@ -0,0 +1,17 @@
+package jeiu.capstone.jongGangHaejo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String username;
+
+    private String password;
+
+}
