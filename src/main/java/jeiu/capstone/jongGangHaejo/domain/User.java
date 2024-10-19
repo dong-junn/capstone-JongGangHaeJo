59e819d023d04721c2715c7ee94bad0b6ed8cbb1
dong-junn <djl020326@gmail.com>
2024-10-19 17:01:24 +0900

    회원가입을 위한 Domain과 Repisoty 추가

--- src/main/java/jeiu/capstone/jongGangHaejo/domain/User.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/domain/User.java
@@ -1,17 +1,14 @@
 package jeiu.capstone.jongGangHaejo.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
-public class Users {
+@Table(name = "users")
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String username;
-
     private String password;
 
 }
