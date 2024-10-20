f67489daaaac9d54668245d1ebc0f111ba13b897
dong-junn <djl020326@gmail.com>
2024-10-21 02:23:36 +0900

    RepositoryTest -> PostRepositoryTest 로 수정

--- src/test/java/jeiu/capstone/jongGangHaejo/repository/PostRepositoryTest.java
+++ src/test/java/jeiu/capstone/jongGangHaejo/repository/PostRepositoryTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class RepositoryTest {
+public class PostRepositoryTest {
 
     @Autowired
     private PostRepository postRepository;
