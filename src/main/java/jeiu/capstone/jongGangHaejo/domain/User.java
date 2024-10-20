47e733962d1625cd299fd91bbe116b8a09ed0533
dong-junn <djl020326@gmail.com>
2024-10-21 01:59:33 +0900

    User domain수정: 우선 ID, PASSWORD만 두고 개발

--- src/main/java/jeiu/capstone/jongGangHaejo/domain/User.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/domain/User.java
@@ -12,12 +12,6 @@ public class User {
     @Column(name = "users_id")
     private Long id; //아이디
 
-    @Column(name = "users_username")
-    private String username; //이름
-
-    @Column(name = "users_email")
-    private String email; //이메일
-
     @Column(name = "users_password")
     private String password; //비밀번호
 }
