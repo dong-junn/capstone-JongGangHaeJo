f9378d5ad2318b22668674fe7acd0b827b4042a1
dong-junn <djl020326@gmail.com>
2024-10-21 00:00:10 +0900

    User Domain Column매핑

--- src/main/java/jeiu/capstone/jongGangHaejo/domain/User.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/domain/User.java
@@ -1,6 +1,7 @@
 package jeiu.capstone.jongGangHaejo.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 
 @Entity
 @Table(name = "users")
@@ -8,7 +9,15 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String username;
-    private String password;
+    @Column(name = "users_id")
+    private Long id; //아이디
 
+    @Column(name = "users_username")
+    private String username; //이름
+
+    @Column(name = "users_email")
+    private String email; //이메일
+
+    @Column(name = "users_password")
+    private String password; //비밀번호
 }
