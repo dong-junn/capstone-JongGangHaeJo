697fcea6c3ae03d4a7a1cd79ba882658806f1ab2
dong-junn <djl020326@gmail.com>
2024-10-21 19:02:34 +0900

    UserRepositoryTest : 비밀번호 해쉬화 하여 DB에 날아가는지 확인 완료

--- src/test/java/jeiu/capstone/jongGangHaejo/repository/UserRepositoryTest.java
+++ src/test/java/jeiu/capstone/jongGangHaejo/repository/UserRepositoryTest.java
@@ -1,28 +1,34 @@
 package jeiu.capstone.jongGangHaejo.repository;
 
+import jeiu.capstone.jongGangHaejo.config.SecurityConfig;
 import jeiu.capstone.jongGangHaejo.domain.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
+@Rollback(false)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private SecurityConfig securityConfig;
 
     @Test
-    void 리파지토리_기본_테스트() {
+    void 리파지토리_비밀번호_해시화_테스트() {
         //given
         User user = User.builder()
                 .id("testID")
-                .password("testPWD")
+                .password(securityConfig.bCryptPasswordEncoder().encode("testPassword"))
                 .build();
 
 
@@ -31,7 +37,7 @@ class UserRepositoryTest {
 
         //then
         Assertions.assertThat(user.getId()).isEqualTo("testID");
-        Assertions.assertThat(user.getPassword()).isEqualTo("testPWD");
+        assertTrue(securityConfig.bCryptPasswordEncoder().matches("testPassword", savedUser.getPassword()));
 
     }
 
