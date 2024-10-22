6ad6861b8099941f42d90f01a21debc0713fbc6c
dong-junn <djl020326@gmail.com>
2024-10-22 14:46:18 +0900

    UserController, UserService 껍데기만 우선 만들어 두기

---
+++ src/main/java/jeiu/capstone/jongGangHaejo/controller/UserController.java
@@ -0,0 +1,8 @@
+package jeiu.capstone.jongGangHaejo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+}
---
+++ src/main/java/jeiu/capstone/jongGangHaejo/service/UserService.java
@@ -0,0 +1,7 @@
+package jeiu.capstone.jongGangHaejo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
