caddcf7f0d9b13c23bacb5f2525548218ba5b103
dong-junn <djl020326@gmail.com>
2024-10-26 03:43:21 +0900

    User관련 코드 삭제

--- src/main/java/jeiu/capstone/jongGangHaejo/repository/UserRepisotory.java
+++
@@ -1,7 +0,0 @@
-package jeiu.capstone.jongGangHaejo.repository;
-
-import jeiu.capstone.jongGangHaejo.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepisotory extends JpaRepository<User, Long> {
-}
--- src/main/java/jeiu/capstone/jongGangHaejo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package jeiu.capstone.jongGangHaejo.repository;
-
-import jeiu.capstone.jongGangHaejo.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
