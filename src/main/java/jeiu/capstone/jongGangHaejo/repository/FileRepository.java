20b8ff4f281d80db3cbe7ec308b6760768a9b438
dong-junn <djl020326@gmail.com>
2024-10-27 12:49:45 +0900

    FileRepository 추가

---
+++ src/main/java/jeiu/capstone/jongGangHaejo/repository/FileRepository.java
@@ -0,0 +1,7 @@
+package jeiu.capstone.jongGangHaejo.repository;
+
+import jeiu.capstone.jongGangHaejo.domain.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository<File, Long> {
+}
