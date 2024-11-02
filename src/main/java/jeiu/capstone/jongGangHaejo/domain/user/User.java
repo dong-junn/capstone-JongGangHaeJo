3096b9c614102bf1c71f5e9a1300742fadd8ac96
dong-junn <djl020326@gmail.com>
2024-11-02 23:27:33 +0900

    user entity role 추가

--- src/main/java/jeiu/capstone/jongGangHaejo/domain/user/User.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/domain/user/User.java
@@ -1,18 +1,20 @@
-package jeiu.capstone.jongGangHaejo.domain;
+package jeiu.capstone.jongGangHaejo.domain.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter @Setter @ToString
+@Getter @Setter
 public class User {
 
     @Id
     private String id;
     private String password;
     private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Builder
     public User(String id, String password, String name) {
