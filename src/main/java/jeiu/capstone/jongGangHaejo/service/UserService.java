5234f9373a1963f0a5f1161c8af6500b83b973ee
dong-junn <djl020326@gmail.com>
2024-11-02 23:34:44 +0900

    UserService : entity에서 추가한 role 추가

--- src/main/java/jeiu/capstone/jongGangHaejo/service/UserService.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/service/UserService.java
@@ -1,5 +1,6 @@
 package jeiu.capstone.jongGangHaejo.service;
 
+import jeiu.capstone.jongGangHaejo.domain.user.Role;
 import jeiu.capstone.jongGangHaejo.domain.user.User;
 import jeiu.capstone.jongGangHaejo.dto.form.UserFormDto;
 import jeiu.capstone.jongGangHaejo.repository.UserRepository;
@@ -23,6 +24,7 @@ public class UserService {
                 .id(form.getId())
                 .password(passwordEncoder.encode(form.getPassword()))
                 .name(form.getName())
+                .role(Role.USER)
                 .build();
 
         return userRepository.save(user);
