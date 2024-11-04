6de734029bbe610b9cc1b30bb0b47ded21a10f6c
dong-junn <djl020326@gmail.com>
2024-11-05 03:29:19 +0900

    UserConfig : Security의 User 상속한 class

---
+++ src/main/java/jeiu/capstone/jongGangHaejo/config/UserConfig.java
@@ -0,0 +1,19 @@
+package jeiu.capstone.jongGangHaejo.config;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserConfig extends User {
+
+    public UserConfig(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+
+    public UserConfig(jeiu.capstone.jongGangHaejo.domain.user.User user) {
+        super(user.getId(), user.getPassword(), List.of(new SimpleGrantedAuthority("USER")));
+    }
+}
