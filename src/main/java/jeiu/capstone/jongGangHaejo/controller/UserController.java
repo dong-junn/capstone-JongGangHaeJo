7b3237d4fdf63d0e69d8d115be1aa20221a20fe0
dong-junn <djl020326@gmail.com>
2024-11-05 03:01:50 +0900

    UserController : @PostMapping("/sign-in") 추가

--- src/main/java/jeiu/capstone/jongGangHaejo/controller/UserController.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/controller/UserController.java
@@ -3,7 +3,6 @@ package jeiu.capstone.jongGangHaejo.controller;
 import jakarta.validation.Valid;
 import jeiu.capstone.jongGangHaejo.dto.form.user.SignInDto;
 import jeiu.capstone.jongGangHaejo.dto.form.user.SignUpDto;
-import jeiu.capstone.jongGangHaejo.service.user.SignInService;
 import jeiu.capstone.jongGangHaejo.service.user.SignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,21 +19,12 @@ import java.util.Map;
 public class UserController {
 
     private final SignUpService signUpService;
-    private final SignInService signInService;
 
     @PostMapping("/sign-up")
     public Map<String, String> singUp(@RequestBody @Valid SignUpDto form) {
         signUpService.createUser(form);
         Map<String, String> map = new HashMap<>();
         map.put("message", "종강해조 게시판의 회원이 되신 것을 환영합니다!");
-        return map;
-    }
-
-    @PostMapping("/sign-in")
-    public Map<String, String> signIn(@RequestBody @Valid SignInDto form) {
-        signInService.processLogin(form);
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "로그인에 성공 하였습니다");
         return map;
     }
 }
