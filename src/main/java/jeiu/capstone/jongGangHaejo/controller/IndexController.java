2186685db1ce93357b25598baf91f1830886616e
dong-junn <djl020326@gmail.com>
2024-11-05 03:01:09 +0900

    IndexController : 로그인 테스트용 /user 추가

--- src/main/java/jeiu/capstone/jongGangHaejo/controller/IndexController.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/controller/IndexController.java
@@ -1,15 +1,21 @@
 package jeiu.capstone.jongGangHaejo.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.attribute.UserPrincipal;
+
 @RestController
-@RequestMapping("/")
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index() {
         return "종강해조 게시판 메인 페이지";
+    }
+
+    @GetMapping("/user")
+    public String user(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        return "로그인 하면 보여지는 테스트용 컨트롤러";
     }
 }
