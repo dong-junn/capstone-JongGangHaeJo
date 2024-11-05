fef7072e98b5029c19d3f96c7f67b2846e8e258e
dong-junn <djl020326@gmail.com>
2024-11-05 16:25:15 +0900

    IndexController, SecurityController : 로그인 관련 Controller

--- src/main/java/jeiu/capstone/jongGangHaejo/controller/IndexController.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/controller/IndexController.java
@@ -1,11 +1,7 @@
 package jeiu.capstone.jongGangHaejo.controller;
 
-import jeiu.capstone.jongGangHaejo.config.UserConfig;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.nio.file.attribute.UserPrincipal;
 
 @RestController
 public class IndexController {
@@ -15,8 +11,4 @@ public class IndexController {
         return "종강해조 게시판 메인 페이지";
     }
 
-    @GetMapping("/user")
-    public String user(@AuthenticationPrincipal UserConfig user) {
-        return "로그인 회원: " + user.getUsername();
-    }
 }
