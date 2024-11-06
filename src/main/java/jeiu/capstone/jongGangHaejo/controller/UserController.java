7a1af8f1e46432921481635852e2455da162338e
dong-junn <djl020326@gmail.com>
2024-11-07 00:17:50 +0900

    UserController : 로그인시 JWT를 이용하여 로그인하도록 변경

--- src/main/java/jeiu/capstone/jongGangHaejo/controller/UserController.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/controller/UserController.java
@@ -1,9 +1,15 @@
 package jeiu.capstone.jongGangHaejo.controller;
 
 import jakarta.validation.Valid;
+import jeiu.capstone.jongGangHaejo.dto.form.user.SignInDto;
 import jeiu.capstone.jongGangHaejo.dto.form.user.SignUpDto;
+import jeiu.capstone.jongGangHaejo.jwt.JwtUtil;
 import jeiu.capstone.jongGangHaejo.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +23,10 @@ import java.util.Map;
 @RequestMapping("/member")
 public class UserController {
 
+    private final AuthenticationManager authenticationManager;
     private final UserService userService;
+    private final UserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/sign-up")
     public Map<String, String> singUp(@RequestBody @Valid SignUpDto form) {
@@ -25,5 +34,16 @@ public class UserController {
         Map<String, String> map = new HashMap<>();
         map.put("message", "종강해조 게시판의 회원이 되신 것을 환영합니다!");
         return map;
+    }
+
+    @PostMapping("/sign-in")
+    public Map<String, String> signIn(@RequestBody @Valid SignInDto form) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(form.getId(), form.getPassword()));
+        UserDetails user = userDetailsService.loadUserByUsername(form.getId());
+        String token = jwtUtil.generateToken(user);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("token", token);
+        return response;
     }
 }
