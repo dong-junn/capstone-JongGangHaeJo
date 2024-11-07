c30da24fb426a3c2bdb7c268dcfec580c571ce2f
dong-junn <djl020326@gmail.com>
2024-11-08 01:19:51 +0900

    JWT 리팩토링 : 로그인 컨트롤러에서 요청, 응답하는 SignUpResponse, SigninResponse DTO

---
+++ src/main/java/jeiu/capstone/jongGangHaejo/security/dto/response/SignUpResponse.java
@@ -0,0 +1,13 @@
+package jeiu.capstone.jongGangHaejo.security.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class SignUpResponse {
+    private String message;
+}
