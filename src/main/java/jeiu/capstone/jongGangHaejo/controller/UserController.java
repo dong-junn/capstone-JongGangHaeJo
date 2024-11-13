808fb5b6c4b0800e0418ed160357d30b6b0c3454
dong-junn <djl020326@gmail.com>
2024-11-13 20:43:23 +0900

    UserController : 로그인, 회원가입 요청 uri 수정

--- src/main/java/jeiu/capstone/jongGangHaejo/controller/UserController.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member")
+@RequestMapping()
 public class UserController {
 
     private final UserService userService;
