126960da67570d565fd2a86f0dffa785d4757c1e
dong-junn <djl020326@gmail.com>
2024-11-14 18:38:09 +0900

    UserConfig : 다수의 role을 가지도록 코드 수정

--- src/main/java/jeiu/capstone/jongGangHaejo/security/config/UserConfig.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/security/config/UserConfig.java
@@ -1,19 +1,19 @@
 package jeiu.capstone.jongGangHaejo.security.config;
 
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
-import java.util.Collection;
-import java.util.List;
+
+import java.util.stream.Collectors;
 
 public class UserConfig extends User {
-
-    public UserConfig(String username, String password, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, authorities);
-    }
-
     public UserConfig(jeiu.capstone.jongGangHaejo.domain.user.User user) {
-        super(user.getName(), user.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        super(
+                user.getName(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getValue()))
+                        .collect(Collectors.toList())
+        );
     }
 }
