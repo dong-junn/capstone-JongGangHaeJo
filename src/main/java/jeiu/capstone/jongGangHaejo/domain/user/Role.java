643bcff38865ef86af05d635da846e6f5a9e41d7
dong-junn <djl020326@gmail.com>
2024-11-14 18:34:20 +0900

    admin은 user,admin 다수의 role을 가질 수 있도록 entity 수정

--- src/main/java/jeiu/capstone/jongGangHaejo/domain/user/Role.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/domain/user/Role.java
@@ -1,6 +1,16 @@
 package jeiu.capstone.jongGangHaejo.domain.user;
 
 public enum Role {
-    ROLE_ADMIN,
-    ROLE_USER
+    ROLE_USER("ROLE_USER"),
+    ROLE_ADMIN("ROLE_ADMIN");
+
+    private String value;
+
+    Role(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
--- src/main/java/jeiu/capstone/jongGangHaejo/domain/user/User.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/domain/user/User.java
@@ -3,6 +3,9 @@ package jeiu.capstone.jongGangHaejo.domain.user;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter @Setter
@@ -13,14 +16,19 @@ public class User {
     private String password;
     private String name;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id")
+    )
     @Enumerated(EnumType.STRING)
-    private Role role;
+    private Set<Role> roles = new HashSet<>();
 
     @Builder
-    public User(String id, String password, String name, Role role) {
+    public User(String id, String password, String name, Set<Role> roles) {
         this.id = id;
         this.password = password;
         this.name = name;
-        this.role = role;
+        this.roles = roles;
     }
 }
