89c75024a61c94f3e459d15e96114b1369438dc3
dong-junn <djl020326@gmail.com>
2024-11-14 18:43:50 +0900

    AuthController : UserService -> SecurityService 변경에 따른 변경

--- src/main/java/jeiu/capstone/jongGangHaejo/controller/AuthController.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/controller/AuthController.java
@@ -5,7 +5,7 @@ import jeiu.capstone.jongGangHaejo.dto.form.user.SignUpDto;
 import jeiu.capstone.jongGangHaejo.security.dto.LoginDto;
 import jeiu.capstone.jongGangHaejo.security.dto.response.member.SignInResponse;
 import jeiu.capstone.jongGangHaejo.security.dto.response.member.SignUpResponse;
-import jeiu.capstone.jongGangHaejo.security.service.UserService;
+import jeiu.capstone.jongGangHaejo.security.service.SecurityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,17 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping()
 public class AuthController {
 
-    private final UserService userService;
+    private final SecurityService securityService;
 
     @PostMapping("/sign-up")
     public ResponseEntity<SignUpResponse> signUp(@RequestBody SignUpDto signUpDto) {
-        User user = userService.createUser(signUpDto);
+        User user = securityService.createUser(signUpDto);
         return ResponseEntity.ok(new SignUpResponse("회원가입이 완료되었습니다."));
     }
 
     @PostMapping("/sign-in")
     public ResponseEntity<SignInResponse> signIn(@RequestBody LoginDto loginDto) {
-        String token = userService.login(loginDto);
+        String token = securityService.login(loginDto);
         return ResponseEntity.ok(new SignInResponse(token));
     }
 
