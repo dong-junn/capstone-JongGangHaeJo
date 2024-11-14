6ed83a8a19f23aaccbb741a142e131c5d175d5d6
dong-junn <djl020326@gmail.com>
2024-11-14 18:36:37 +0900

    SecurityController : NPE문제 해결하면서 log 추가

--- src/main/java/jeiu/capstone/jongGangHaejo/controller/SecurityController.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/controller/SecurityController.java
@@ -1,15 +1,24 @@
 package jeiu.capstone.jongGangHaejo.controller;
 
 import jeiu.capstone.jongGangHaejo.security.config.UserConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SecurityController {
 
     @GetMapping("/user")
     public String user(@AuthenticationPrincipal UserConfig user) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        log.info("인증정보: {}", auth);
+        log.info("Principal 타입: {}", auth != null ? auth.getPrincipal().getClass() : "null");
+
         return "로그인 회원: " + user.getUsername();
     }
 
