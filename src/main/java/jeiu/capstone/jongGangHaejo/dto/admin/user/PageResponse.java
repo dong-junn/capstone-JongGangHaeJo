adc2ea5ba4ca07f8a8e440c221532222cd6201d4
dong-junn <djl020326@gmail.com>
2024-11-15 21:49:15 +0900

    admin user 조회도 /post 규약에 맞추어 나오도록 진행

--- src/main/java/jeiu/capstone/jongGangHaejo/dto/admin/user/PageResponse.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/dto/admin/user/PageResponse.java
@@ -8,16 +8,16 @@ import java.util.List;
 @Getter
 public class PageResponse<T> {
     private final List<T> content;
-    private final int pageNumber;
-    private final int pageSize;
+    private final int page;
+    private final int size;
     private final long totalElements;
     private final int totalPages;
     private final boolean last;
 
     public PageResponse(Page<T> page) {
         this.content = page.getContent();
-        this.pageNumber = page.getNumber() + 1; //application.yml에서 설정한 1페이지부터 시작에 맞추어 수정
-        this.pageSize = page.getSize();
+        this.page = page.getNumber() + 1; //application.yml에서 설정한 1페이지부터 시작에 맞추어 수정
+        this.size = page.getSize();
         this.totalElements = page.getTotalElements();
         this.totalPages = page.getTotalPages();
         this.last = page.isLast();
