1db6bed47f20cb3f7e01a42b247f4c1de9a154b8
wj <6548lha@naver.com>
2024-11-16 03:30:01 +0900

    이전에 사용했던 이메일 인증코드 자동 삭제하는 어노테이션

--- src/main/java/jeiu/capstone/jongGangHaejo/JongGangHaejoApplication.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/JongGangHaejoApplication.java
@@ -2,8 +2,10 @@ package jeiu.capstone.jongGangHaejo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JongGangHaejoApplication {
 
 	public static void main(String[] args) {
