8894c11d1ffad84675487f5920bfe636dd3f6182
wj <6548lha@naver.com>
2024-11-17 21:35:14 +0900

    회원 정보 찾기 추가

--- src/main/java/jeiu/capstone/jongGangHaejo/repository/UserRepository.java
+++ src/main/java/jeiu/capstone/jongGangHaejo/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package jeiu.capstone.jongGangHaejo.repository;
 
 import jeiu.capstone.jongGangHaejo.domain.user.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByNameAndEmail(String name, String email);
+    Optional<User> findByIdAndEmail(String id, String email);
+    Optional<User> findByEmail(String email);
 }
